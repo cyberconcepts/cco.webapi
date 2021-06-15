f3db4c8d97fa564144a46c9a418e0e7d0cf90313
Helmut Merz <helmutm@cy55.de>
2021-06-15 14:42:59 +0200

    add postMessage() function for sending messages to integrator

--- src/cco/webapi/client.py
+++ src/cco/webapi/client.py
@@ -16,10 +16,20 @@ from cco.processor import hook
 logger = getLogger('cco.webapi.client')
 
 
+def postMessage(baseUrl, domain='system', action='data', class_='', item='',
+        payload=None, cred=None):
+    url = '/'.join(p for p in (baseUrl, domain, action, class_, item) if p)
+    resp = requests.post(url, data=payload, auth=cred, timeout=10)
+    logger.info('postMessage: %s %s.' % (url, resp.status_code))
+    # TODO: check resp.status_code
+    #return resp.json(), dict(state='success')
+    return r.content
+    
+
 def postJson(url, data, cred):
     resp = requests.post(url, json=data, auth=cred, timeout=10)
-    print '*** reponse', resp.text
-    print '*** status', resp.status_code
+    logger.info('postJson: %s %s.' % (resp.status_code, resp.text))
+    # TODO: check resp.status_code
     return resp.json(), dict(state='success')
 
 
@@ -33,7 +43,7 @@ def notify(obj, data):
     baseUrl = config.get('url', 'http://localhost:8123')
     cred = config.get('_credentials', ('dummy', 'dummy')) 
     url = '/'.join((baseUrl, obj._hook_message_base, obj.identifier))
-    print '*** notify', url, data, cred
+    logger.info('notify: %s - %s - %s.' % (url, data, cred))
     postJson(url, data, cred)
 
 
